c29f629a1ee367e32045f52bdd1d7707a82cfb26
MalMWare <malkelleymartin@gmail.com>
2023-06-01 02:06:09 -0700

    added blueprint routes

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -3,6 +3,10 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    # register pet blueprint 
+    from . import pet
+    app.register_blueprint(pet.bp)
+
     @app.route('/')
     def hello():
         return "Hello, Petfax!"
